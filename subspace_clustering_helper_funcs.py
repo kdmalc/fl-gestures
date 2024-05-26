45cb999e6a1c7aa051381f3d8ff143f7be101b1f
kdmalc <kdmalc@gmail.com>
2024-05-26 10:55:33 -0500

    EMG PCA Clustering Gauntlet

--- subspace_clustering_helper_funcs.py
+++ subspace_clustering_helper_funcs.py
@@ -5,12 +5,78 @@ import copy
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
-from sklearn.decomposition import IncrementalPCA
-from sklearn.decomposition import KernelPCA
-from sklearn.manifold import MDS
-from sklearn.manifold import Isomap
+from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
+from sklearn.cluster import KMeans
+from sklearn.neighbors import NearestNeighbors
+# Have not been tested/implemented yet:
+from sklearn.decomposition import IncrementalPCA, KernelPCA
+from sklearn.manifold import MDS, Isomap
 
 #from umap import UMAP
+
+
+def dunn_index(X, labels):
+    # Compute pairwise distances between points in the same cluster
+    intra_cluster_distances = []
+    for cluster_label in np.unique(labels):
+        cluster_points = X[labels == cluster_label]
+        if len(cluster_points) > 1:
+            nearest_neighbors = NearestNeighbors(n_neighbors=2).fit(cluster_points)
+            distances, _ = nearest_neighbors.kneighbors()
+            intra_cluster_distances.extend(distances[:, 1])
+
+    # Compute pairwise distances between points in different clusters
+    inter_cluster_distances = []
+    for i in range(len(X)):
+        for j in range(i + 1, len(X)):
+            if labels[i] != labels[j]:
+                inter_cluster_distances.append(np.linalg.norm(X[i] - X[j]))
+
+    # Compute Dunn Index
+    min_inter_cluster_distance = min(inter_cluster_distances)
+    max_intra_cluster_distance = max(intra_cluster_distances)
+    return min_inter_cluster_distance / max_intra_cluster_distance
+
+def gap_statistics(X, labels):
+    # Compute within-cluster dispersion
+    Wk = np.sum([np.mean(np.linalg.norm(X[np.where(labels == k)] - np.mean(X[np.where(labels == k)], axis=0), axis=1)) for k in np.unique(labels)])
+
+    # Generate reference datasets and compute within-cluster dispersion for each
+    reference_Wk = []
+    for i in range(10):
+        random_data = np.random.rand(*X.shape)
+        reference_labels = KMeans(n_clusters=len(np.unique(labels))).fit_predict(random_data)
+        reference_Wk.append(np.sum([np.mean(np.linalg.norm(random_data[np.where(reference_labels == k)] - np.mean(random_data[np.where(reference_labels == k)], axis=0), axis=1)) for k in np.unique(reference_labels)]))
+    
+    # Compute Gap Statistics
+    gap = np.mean(np.log(reference_Wk)) - np.log(Wk)
+    return gap
+
+def run_clustering_algorithm(clustering_algo_str, X, num_clusters=2, eps=0.5, min_samples=5):
+    if clustering_algo_str == 'KMeans':
+        model = KMeans(n_clusters=num_clusters, random_state=42, n_init='auto') 
+        # ^ Read about auto, basically controls how many times it is rerun
+    elif clustering_algo_str == 'DBSCAN':
+        #It's suitable for datasets with complex cluster shapes and noise.
+        #However, it may struggle with clusters of varying densities or high-dimensional data.
+        #--> Probably both are true for us?...
+        model = DBSCAN(eps=eps, min_samples=min_samples)
+    elif clustering_algo_str == 'OPTICS':
+        #It's suitable for datasets with varying cluster densities and non-convex shapes.
+        #It can handle noise and doesn't require specifying the number of clusters in advance.
+        model = OPTICS(eps=eps, min_samples=min_samples)
+
+    labels = model.fit_predict(X)
+
+    # Compute clustering metrics
+    silhouette = silhouette_score(X, labels)
+    db_index = davies_bouldin_score(X, labels)
+    ch_index = calinski_harabasz_score(X, labels)
+    di_index = dunn_index(X, labels)
+    gap_stat = gap_statistics(X, labels)
+
+    return silhouette, db_index, ch_index, di_index, gap_stat
+
 
 
 def apply_model(model_str, input_df, num_dims, hp):
