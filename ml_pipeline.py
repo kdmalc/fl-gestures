b02e68a7ad8ab4cb86020a62971f66e06a2111ba
Kai Malcolm <kdmalc@gmail.com>
2024-05-31 15:58:07 -0500

    One hot encoded gesture labels

--- ml_pipeline.py
+++ ml_pipeline.py
@@ -4,3 +4,16 @@ import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 
 
+class GestureDatasetClustering(Dataset):
+    def __init__(self, data_tensor, label_tensor):
+        self.data = data_tensor
+        self.labels = label_tensor
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        x = self.data[idx]
+        y = self.labels[idx]
+        return x, y
+
