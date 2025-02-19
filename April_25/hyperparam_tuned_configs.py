8c28cf435e1218f761bbf08fbfbd8733f7318f1c
Kai Malcolm <kdmalc@gmail.com>
2025-02-19 12:45:47 -0600

    Added base config to configs file

--- April_25/hyperparam_tuned_configs.py
+++ April_25/hyperparam_tuned_configs.py
@@ -1,13 +1,13 @@
+import copy
 from datetime import datetime
 timestamp = datetime.now().strftime("%Y%m%d_%H%M")
-NUM_CHANNELS = 16  # NOT USED WITH ELEC573Net rn
-NUM_TRAIN_GESTURES = 8 
-NUM_FT_GESTURES = 1  # Oneshot
+NUM_CHANNELS = 16  # Note that ELEC573Net is hardcoded in, doesn't use this...
+NUM_TRAIN_GESTURES = 8  # For pretrained models
+NUM_FT_GESTURES = 1  # Oneshot finetuning
 
 
-# This one has not actually been hyperparamtuned...
-DynamicCNNLSTM_config = {
-    "model_str": "CNNLSTM",
+# Base configuration, shared between all other configs (unless they get overwritten)
+base_config = {
     "feature_engr": None, 
     "time_steps": 1,
     "sequence_length": 64,
@@ -16,30 +16,17 @@ DynamicCNNLSTM_config = {
     "num_pretrain_users": 24, 
     "num_testft_users": 8, 
     "weight_decay": 0.0,
-    "fc_layers": [128, 64],
     "fc_dropout": 0.3,
-    "dense_cnnlstm_dropout": 0.3,
-    "conv_layers": [
-        [64, 3, 1],
-        [128, 3, 1]],
-    "cnn_dropout": 0.3,
+    "cnn_dropout": 0.0,
     "batch_size": 16,
-    "added_dense_ft_hidden_size": 64, 
-    "verbose": False,
-    "use_dense_cnn_lstm": False,  # Not used with this network!
     "timestamp": timestamp,
-    "pooling_layers": [True, True, True, True],
     "optimizer": "adam",
     "num_epochs": 100,
     "num_classes": 10,
     "num_channels": NUM_CHANNELS,
-    "user_split_json_filepath": "April_25\\fixed_user_splits\\24_8_user_splits.json",  # Mystery seed. Rerun with other user splits... ought to kfcv...
-    "results_save_dir": f"C:\\Users\\kdmen\\Repos\\fl-gestures\\April_25\\results\\{timestamp}",  # \\hyperparam_tuning
-    "models_save_dir": f"C:\\Users\\kdmen\\Repos\\fl-gestures\\April_25\\models\\{timestamp}",  # \\hyperparam_tuning
-    "lstm_num_layers": 1,
-    "lstm_hidden_size": 8,
-    "lstm_dropout": 0.8,
-    "log_each_pid_results": False,
+    "user_split_json_filepath": "April_25\\fixed_user_splits\\24_8_user_splits.json",
+    "results_save_dir": f"C:\\Users\\kdmen\\Repos\\fl-gestures\\April_25\\results\\{timestamp}",
+    "models_save_dir": f"C:\\Users\\kdmen\\Repos\\fl-gestures\\April_25\\models\\{timestamp}",
     "learning_rate": 0.001,
     "finetune_strategy": "progressive_unfreeze",
     "progressive_unfreezing_schedule": 5,
@@ -47,215 +34,107 @@ DynamicCNNLSTM_config = {
     "ft_learning_rate": 0.001,
     "ft_batch_size": 10,
     "num_ft_epochs": 100,
-    "save_ft_models": False,
     "use_earlystopping": True,
-    #"lr_scheduler_gamma": 1.0,
-    "lr_scheduler_patience": 4, 
+    "lr_scheduler_patience": 4,
     "lr_scheduler_factor": 0.1, 
     "earlystopping_patience": 6,
-    "earlystopping_min_delta": 0.01
-}
-DynamicCNN_config = {
-    "model_str": "CNN",
-    "feature_engr": None, 
-    "time_steps": 1,
-    "sequence_length": 64,
-    "num_train_gesture_trials": NUM_TRAIN_GESTURES, 
-    "num_ft_gesture_trials": NUM_FT_GESTURES,
-    "num_pretrain_users": 24, 
-    "num_testft_users": 8, 
-    "weight_decay": 0.0,
-    "fc_layers": [128, 64],
-    "fc_dropout": 0.3,
-    "conv_layers": [
-        [64, 3, 1],
-        [128, 3, 1]],
-    "cnn_dropout": 0.3,
-    "batch_size": 16,
-    "added_dense_ft_hidden_size": 64, 
+    "earlystopping_min_delta": 0.01, 
+    "ft_lr_scheduler_patience": 6, 
+    "ft_lr_scheduler_factor": 0.1, 
+    "ft_earlystopping_patience": 8,
+    "ft_earlystopping_min_delta": 0.01,
     "verbose": False,
-    "timestamp": timestamp,
-    "pooling_layers": [True, True, True, True],
-    "optimizer": "adam",
-    "num_epochs": 100,
-    "num_classes": 10,
-    "num_channels": NUM_CHANNELS,
-    "user_split_json_filepath": "April_25\\fixed_user_splits\\24_8_user_splits.json",  # Mystery seed. Rerun with other user splits... ought to kfcv...
-    "results_save_dir": f"C:\\Users\\kdmen\\Repos\\fl-gestures\\April_25\\results\\{timestamp}",  # \\hyperparam_tuning
-    "models_save_dir": f"C:\\Users\\kdmen\\Repos\\fl-gestures\\April_25\\models\\{timestamp}",  # \\hyperparam_tuning
-    "log_each_pid_results": False,
-    "learning_rate": 0.001,
-    "finetune_strategy": "progressive_unfreeze",
-    "progressive_unfreezing_schedule": 5,
-    "ft_weight_decay": 0.0,
-    "ft_learning_rate": 0.001,
-    "ft_batch_size": 10,
-    "num_ft_epochs": 100,
     "save_ft_models": False,
-    "use_earlystopping": True,
-    #"lr_scheduler_gamma": 1.0,
-    "lr_scheduler_patience": 4, 
-    "lr_scheduler_factor": 0.1, 
-    "earlystopping_patience": 6,
-    "earlystopping_min_delta": 0.01
+    "log_each_pid_results": False
 }
 
 
-DynamicMomonaNetR_config = {
+DynamicMomonaNetR_config = copy.deepcopy(base_config)
+DynamicMomonaNetR_config.update({
     "model_str": "DynamicMomonaNet",
-    "num_train_gesture_trials": NUM_TRAIN_GESTURES, 
-    "num_ft_gesture_trials": NUM_FT_GESTURES,
-    "num_pretrain_users": 24, 
-    "num_testft_users": 8, 
-    "feature_engr": None, 
-    "weight_decay": 1e-4,
-    "fc_layers": [128, 64],
-    "fc_dropout": 0.4,
     "dense_cnnlstm_dropout": 0.3,
     "conv_layers": [
         [64, 3, 1],
         [128, 3, 1]],
-    "cnn_dropout": 0.3,
-    "batch_size": 16,
-    "verbose": False,
-    "use_dense_cnn_lstm": True,
-    "timestamp": timestamp,
-    "time_steps": None,
-    "sequence_length": 64,
     "pooling_layers": [True, True, True, True],
-    "optimizer": "adam",
-    "num_epochs": 100,
-    "num_classes": 10,
-    "num_channels": NUM_CHANNELS,
-    "user_split_json_filepath": "April_25\\fixed_user_splits\\24_8_user_splits.json",  # Mystery seed. Rerun with other user splits... ought to kfcv...
-    "results_save_dir": f"C:\\Users\\kdmen\\Repos\\fl-gestures\\April_25\\results\\{timestamp}",  # \\hyperparam_tuning
-    "models_save_dir": f"C:\\Users\\kdmen\\Repos\\fl-gestures\\April_25\\models\\{timestamp}",  # \\hyperparam_tuning
     "lstm_num_layers": 1,
     "lstm_hidden_size": 64,
-    "lstm_dropout": 0.5,  # When there is only 1 LSTM layer, dropout doesn't do anything!
-    "log_each_pid_results": False,
-    "learning_rate": 0.001,
-    "finetune_strategy": "progressive_unfreeze",
-    "progressive_unfreezing_schedule": 4,
-    "ft_weight_decay": 0.001,
-    "ft_learning_rate": 0.0001,
-    "ft_batch_size": 10,
-    "num_ft_epochs": 100,
-    "save_ft_models": False,
-    "use_earlystopping": True,
-    #"lr_scheduler_gamma": 1.0,
-    "lr_scheduler_patience": 4, 
-    "lr_scheduler_factor": 0.1, 
-    "earlystopping_patience": 6,
-    "earlystopping_min_delta": 0.01,
-    "ft_lr_scheduler_patience": 6, 
-    "ft_lr_scheduler_factor": 0.1, 
-    "ft_earlystopping_patience": 8,
-    "ft_earlystopping_min_delta": 0.01
-}
-
-
-DynamicMomonaNet_config = {
-    "model_str": "DynamicMomonaNet",
-    "num_train_gesture_trials": NUM_TRAIN_GESTURES, 
-    "num_ft_gesture_trials": NUM_FT_GESTURES,
-    "num_pretrain_users": 24, 
-    "num_testft_users": 8, 
-    "feature_engr": None, 
-    "weight_decay": 0.0,
+    "lstm_dropout": 0.0,  # When there is only 1 LSTM layer, dropout doesn't do anything!
     "fc_layers": [128, 64],
-    "fc_dropout": 0.3,
+    "added_dense_ft_hidden_size": 64, 
+    "use_dense_cnn_lstm": True,  
+})
+DynamicMomonaNetR_config["weight_decay"] = 1e-4
+DynamicMomonaNetR_config["fc_dropout"] = 0.4
+DynamicMomonaNetR_config["time_steps"] = None
+DynamicMomonaNetR_config["progressive_unfreezing_schedule"] = 4
+DynamicMomonaNetR_config["ft_weight_decay"] = 0.001
+DynamicMomonaNetR_config["ft_learning_rate"] = 0.0001
+
+
+DynamicMomonaNet_config = copy.deepcopy(base_config)
+DynamicMomonaNet_config.update({
+    "model_str": "DynamicMomonaNet",
     "dense_cnnlstm_dropout": 0.3,
     "conv_layers": [
         [64, 3, 1],
         [128, 3, 1]],
-    "cnn_dropout": 0.3,
-    "batch_size": 16,
-    "verbose": False,
-    "use_dense_cnn_lstm": True,
-    "timestamp": timestamp,
-    "time_steps": None,
-    "sequence_length": 64,
     "pooling_layers": [True, True, True, True],
-    "optimizer": "adam",
-    "num_epochs": 100,
-    "num_classes": 10,
-    "num_channels": NUM_CHANNELS,
-    "user_split_json_filepath": "April_25\\fixed_user_splits\\24_8_user_splits.json",  # Mystery seed. Rerun with other user splits... ought to kfcv...
-    "results_save_dir": f"C:\\Users\\kdmen\\Repos\\fl-gestures\\April_25\\results\\{timestamp}",  # \\hyperparam_tuning
-    "models_save_dir": f"C:\\Users\\kdmen\\Repos\\fl-gestures\\April_25\\models\\{timestamp}",  # \\hyperparam_tuning
     "lstm_num_layers": 1,
     "lstm_hidden_size": 8,
-    "lstm_dropout": 0.8,  # When there is only 1 LSTM layer, dropout doesn't do anything!
-    "log_each_pid_results": False,
-    "learning_rate": 0.001,
-    "finetune_strategy": "progressive_unfreeze",
-    "progressive_unfreezing_schedule": 5,
-    "ft_weight_decay": 0.0,
-    "ft_learning_rate": 0.001,
-    "ft_batch_size": 10,
-    "num_ft_epochs": 100,
-    "save_ft_models": False,
-    "use_earlystopping": True,
-    #"lr_scheduler_gamma": 1.0,
-    "lr_scheduler_patience": 4, 
-    "lr_scheduler_factor": 0.1, 
-    "earlystopping_patience": 6,
-    "earlystopping_min_delta": 0.01,
-    "ft_lr_scheduler_patience": 6, 
-    "ft_lr_scheduler_factor": 0.1, 
-    "ft_earlystopping_patience": 8,
-    "ft_earlystopping_min_delta": 0.01
-}
+    "lstm_dropout": 0.0,
+    "fc_layers": [128, 64],
+    "added_dense_ft_hidden_size": 64, 
+    "use_dense_cnn_lstm": False,  
+})
 
 
-# BEST PARAMS FOR GENERIC MODEL
-ELEC573Net_config = {
+ELEC573Net_config = copy.deepcopy(base_config)
+ELEC573Net_config.update({
     "model_str": "ELEC573Net",
-    "num_train_gesture_trials": NUM_TRAIN_GESTURES, 
-    "num_ft_gesture_trials": NUM_FT_GESTURES,
-    "feature_engr": "moments", 
-    "learning_rate": 0.0001,
-    "batch_size": 32,
-    "optimizer": "adam",
-    "weight_decay": 1e-4,
-    "num_channels": 80,  #(int): Number of input channels.
-    "sequence_length": 64,  # Not used for this one AFAIK
-    "time_steps": None,  # Not used for this one AFAIK
-    "num_classes": 10, #(int): Number of output classes.
-    "num_epochs": 50,  # To use earlystopping or not... I guess I can check both...
-    "conv_layers": [(32, 5, 2), (64, 5, 2), (128, 5, 2)], 
-    "fc_layers": [128],  #(list of int): List of integers specifying the sizes of fully connected layers.
-    "use_batchnorm": True,
-    "padding": 1,
+    "conv_layers": [
+        [64, 3, 1],
+        [128, 3, 1]],
+    "fc_layers": [128],
+    "use_batchnorm": True,  # I don't think this exists / is implemented rn?
+    "padding": 1 , # I don't think this is implemented rn...
     "pooling_layers": None,  # In ELEC573Net pooling is on by default, see below. Should this be set to None or...
-    "maxpool": 1,  # Just adding/passing this through 
-    "cnn_dropout": 0.0,
-    "fc_dropout": 0.3, 
-    # DEFAULT PARAMS FROM fine_tune_model() IN OLD CODE: num_epochs=20, lr=0.00001, use_weight_decay=True, weight_decay=0.05)
-    "ft_learning_rate": 0.00001,
-    "ft_weight_decay": 0.05,  # This is really high...
-    "num_ft_epochs": 20,
-    "finetune_strategy": "progressive_unfreeze",
-    "progressive_unfreezing_schedule": 5,
-    # UNSURE ABOUT HOW THIS WORKS
-    "ft_batch_size": 32,  # How was this using 32 as its batch_size if there's only 30 total finetuning gestures...
-    # METADATA
-    "user_split_json_filepath": "April_25\\fixed_user_splits\\24_8_user_splits.json",  # Mystery seed. Rerun with other user splits... ought to kfcv...
-    "results_save_dir": f"C:\\Users\\kdmen\\Repos\\fl-gestures\\April_25\\results\\{timestamp}",  # \\hyperparam_tuning
-    "models_save_dir": f"C:\\Users\\kdmen\\Repos\\fl-gestures\\April_25\\models\\{timestamp}",  # \\hyperparam_tuning
-    "timestamp": timestamp,
-    "verbose": False,
-    "log_each_pid_results": False, 
-    "save_ft_models": False,  # Not even applicable here
-    "use_earlystopping": True,
-    #"lr_scheduler_gamma": 1.0,
-    "lr_scheduler_patience": 4, 
-    "lr_scheduler_factor": 0.1, 
-    "earlystopping_patience": 6,
-    "earlystopping_min_delta": 0.01,
-    "ft_lr_scheduler_patience": 6, 
-    "ft_lr_scheduler_factor": 0.1, 
-    "ft_earlystopping_patience": 8,
-    "ft_earlystopping_min_delta": 0.01
-}
+    "maxpool": 1,  # Just adding/passing this through --> Does this do anything...
+})
+ELEC573Net_config["batch_size"] = 32
+ELEC573Net_config["ft_batch_size"] = 32  # How was this using 32 as its batch_size if there's only 30 total finetuning gestures...
+ELEC573Net_config["num_channels"] = 80
+ELEC573Net_config["cnn_dropout"] = 0.0
+ELEC573Net_config["ft_learning_rate"] = 0.00001
+ELEC573Net_config["ft_weight_decay"] = 0.05  # This is really high...
+ELEC573Net_config["num_ft_epochs"] = 20
+ELEC573Net_config["finetune_strategy"] = "full"
+
+
+DynamicCNNLSTM_config = copy.deepcopy(base_config)
+DynamicCNNLSTM_config.update({
+    "model_str": "CNNLSTM",
+    "dense_cnnlstm_dropout": 0.3,
+    "conv_layers": [
+        [64, 3, 1],
+        [128, 3, 1]],
+    "pooling_layers": [True, True, True, True],
+    "lstm_num_layers": 1,
+    "lstm_hidden_size": 8,
+    "lstm_dropout": 0.0,
+    "fc_layers": [128, 64],
+    "added_dense_ft_hidden_size": 64, 
+    "use_dense_cnn_lstm": False,  
+})
+
+
+DynamicCNN_config = base_config.copy()
+DynamicCNN_config.update({
+    "model_str": "CNN",
+    "conv_layers": [
+        [64, 3, 1],
+        [128, 3, 1]],
+    "pooling_layers": [True, True, True, True],
+    "fc_layers": [128, 64],
+})
+DynamicCNN_config["cnn_dropout"] = 0.3
